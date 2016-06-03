9503d599a9b63e7a83f654b23d6c1b29f686f49a
Mike Peterson <yetep79@gmail.com>
2016-06-03 11:40:43 -0700

    Update TIL_Bot.py

--- TIL_Bot.py
+++ TIL_Bot.py
@@ -1,47 +1,56 @@
 #! /usr/bin/env python3
 import json
 from urllib.request import urlopen,  Request
-import datetime
 import tweepy
 import time
 from tweepy.parsers import JSONParser
+import re
+import Keys
 
+
+#  Authenticate twitter account
+def _twit_auth():
+    consumer_key = Keys.consumer_key
+    consumer_secret = Keys.consumer_secret
+    access_key = Keys.access_key
+    access_secret = Keys.access_secret
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth, parser=JSONParser())
+
+    return api
 
 #  Pulls JSON data for the front page of TIL
 def get_hot_TIL():
+
     url = 'https://www.reddit.com/r/todayilearned/hot/.json'
     hed = { 'User-Agent' : 'a bot to pull hot TIL facts by /u/yetep'}
     req = Request(url,  headers=hed)
     dict = json.loads(urlopen(req).readall().decode('utf-8'))
+
     return dict
 
 #  Parse the JSON and return a dictionary with the score, the post ID, and the title
 def parse_TIL_data(data):
       
-    parsed_data = {'data':[]}
+    parsed_data = []
       
     for post in data['data']['children']:
         if post['data']['author'] != 'TILMods':
-            parsed_data['data'].append({'score':post['data']['score'],  'title':post['data']['title'],  'id':post['data']['id'], 'url':post['data']['url']})
+            parsed_data.append({'score':post['data']['score'],  'title':post['data']['title'],  'id':post['data']['id'], 'url':post['data']['url']})
           
     return parsed_data
 
-#  Authenticate twitter account
-def _twit_auth():
-    
-    consumer_key = 'Your consumer key'
-    consumer_secret = 'Your consumer secret'
-    access_key = 'Your access key'
-    access_secret = 'your access secret'
-    auth = tweepy.OAuthHandler(consumer_key,  consumer_secret)
-    auth.set_access_token(access_key,  access_secret)
-    api = tweepy.API(auth, parser=JSONParser())
-    
-    return api
-
 #  Formats the title for readibility
 def _format_title(msg):
-    TIL = msg['title'].replace('TIL ', '').replace('TIL: ', '')
+
+    _strip = re.compile(r'TIL(:)? (of|that|-)?( )?')
+    _tostrip = _strip.search(msg['title'])
+
+    try:
+        TIL = msg['title'].replace(_tostrip.group(), '')
+    except AttributeError:
+        TIL = msg['title']
 
     if TIL[-1] != '.' and TIL[-2:] != '."':
         TIL = TIL + '.'
@@ -50,11 +59,6 @@ def _format_title(msg):
         TIL = TIL[0] + TIL[1].capitalize() + TIL[2:]
     else:
         TIL = TIL[0].capitalize() + TIL[1:]
-
-    if TIL[0:2] == 'Of':
-        TIL = TIL[3].capitalize() + TIL[4:]
-    elif TIL[0:4] == 'That':
-        TIL = TIL[5].capitalize() + TIL[6:]
 
     return TIL
 
@@ -86,13 +90,13 @@ def main():
     
 # This opens the log of previous post ID's and creates a list of them to check against the new data pulled.  If the log does not exist the list is set to be emply
 
-    log_dict = {'data': []}
+    log_dict = []
 
     try:
         readlog = open('TIL.log',  'rU')
         for line in readlog:
             if line[0] == '{':
-                log_dict['data'].append(json.loads(line))
+                log_dict.append(json.loads(line))
         readlog.close()
     except FileNotFoundError:
         pass
@@ -100,10 +104,8 @@ def main():
 
     log = open('TIL.log', 'a')
 
-    log.write('\n' + str(datetime.datetime.now()) + '\n\n')
-
-    for post in _parse_reddit_data['data']:
-        if post['score'] > 1000 and any(d['id'] == post['id'] for d in log_dict['data']) == False:
+    for post in _parse_reddit_data:
+        if post['score'] > 1000 and any(d['id'] == post['id'] for d in log_dict) == False:
             TILPost = _format_title(post)
             log_data = {'id': post['id'], 'post':TILPost, 'url':post['url']}
             log.write(json.dumps(log_data) + '\n')
